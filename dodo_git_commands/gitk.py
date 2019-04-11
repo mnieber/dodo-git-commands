abec0e226ae066b4b595c999ddf0b34b6fbc3ca1
Maarten Nieber <hallomaarten@yahoo.com>
2019-04-11 13:11:20 +0200

    Add gitk command

---
+++ dodo_git_commands/gitk.py
@@ -0,0 +1,15 @@
+from argparse import ArgumentParser
+from dodo_commands.framework import Dodo, ConfigArg
+
+
+def _args():
+    parser = ArgumentParser(description='Run gitk')
+    args = Dodo.parse_args(parser,
+                           config_args=[ConfigArg('/ROOT/src_dir', 'src_dir')])
+    return args
+
+
+# Use safe=False if the script makes changes other than through Dodo.run
+if Dodo.is_main(__name__, safe=True):
+    args = _args()
+    Dodo.run(['gitk'], cwd=args.src_dir)
