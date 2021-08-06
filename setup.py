7329fec579a1228a672cd2464107d5a0741f5fea
Maarten Nieber <hallomaarten@yahoo.com>
2021-08-06 20:24:21 +0200

    Bump version to 0.4.0 and rename to dodo-git-commands

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(
-    name='dodo_git_commands',
-    version='0.3.0',
+    name='dodo-git-commands',
+    version='0.4.0',
     description='Git related Dodo Commands',
-    url='https://github.com/mnieber/dodo_git_commands',
-    download_url='https://github.com/mnieber/dodo_git_commands/tarball/0.3.0',
+    url='https://github.com/mnieber/dodo-git-commands',
+    download_url='https://github.com/mnieber/dodo-git-commands/tarball/0.4.0',
     author='Maarten Nieber',
     author_email='hallomaarten@yahoo.com',
     license='MIT',
